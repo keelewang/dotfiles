51033c97b11a2812ae38a8b8199745ef917e0650
NullCoke <nullcoke@gmail.com>
2023-03-18 18:18:06 +0800

    tweak fish_escape_delay_ms

--- config.fish
+++ config.fish
@@ -1,13 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -gx http_proxy localhost:7890
-    set -gx https_proxy localhost:7890
 end
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
+# set proxy
+set -gx http_proxy localhost:7890
+set -gx https_proxy localhost:7890
+
+# tweak fish_escape_delay_ms
+set -g fish_escape_delay_ms 300
+
+# init conda
 if test -f $HOME/miniconda3/bin/conda
     eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 end
-# <<< conda initialize <<<
-
