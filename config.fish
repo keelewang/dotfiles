328520bf69e31657f331fe234978e5eba28b84bf
NullCoke <nullcoke@gmail.com>
2023-03-18 17:57:27 +0800

    update

--- config.fish
+++ config.fish
@@ -6,8 +6,8 @@ end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /home/haoyu/miniconda3/bin/conda
-    eval /home/haoyu/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+if test -f $HOME/miniconda3/bin/conda
+    eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 end
 # <<< conda initialize <<<
 
