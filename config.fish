a2dfa18f084c417108b9cc15849c42f147194ac7
NullCoke <nullcoke@gmail.com>
2023-03-22 14:41:09 +0800

    remove conda

--- config.fish
+++ config.fish
@@ -6,10 +6,8 @@ end
 set -gx http_proxy localhost:7891
 set -gx https_proxy localhost:7891
 
+# path
+set -gx JAVA_HOME /usr/java/jdk1.8.0_361-amd64
+
 # tweak fish_escape_delay_ms
 set -g fish_escape_delay_ms 300
-
-# init conda
-if test -f $HOME/miniconda3/bin/conda
-    eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-end
