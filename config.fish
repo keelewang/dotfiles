c7277c3215ac40aae0adb2f0d3712c70004b8bc7
nullcoke <nullcoke@gmail.com>
2023-08-07 08:20:23 +0800

    update

--- config.fish
+++ config.fish
@@ -7,7 +7,6 @@ set -gx http_proxy localhost:7891
 set -gx https_proxy localhost:7891
 
 # path
-set -gx JAVA_HOME /usr/java/jdk1.8.0_361-amd64
 
 # tweak fish_escape_delay_ms
 set -g fish_escape_delay_ms 300
